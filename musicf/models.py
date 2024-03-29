6e353edcdd0cebd1b96ac892b3e0b65645e8febb
barenya09 <barenyamohanty9@gmail.com>
2024-03-29 13:15:00 +0530

    Updated models.py

--- musicf/models.py
+++ musicf/models.py
@@ -1,1 +1,17 @@
 # models here
+from flask_login import UserMixin
+from datetime import datetime
+
+from musicf import db, login_manager
+
+@login_manager.user_loader
+def get_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    profile_pic = db.Column(db.String(50), nullable=False, default="default.jpg")
+    date_created = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow)
