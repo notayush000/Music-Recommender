34d2c3ca5feeb46bc0801cfc38749c104f866105
priyansh221103 <gupta.priyanshu2211@gmail.com>
2024-04-05 12:16:33 +0530

    added recommendation and load music

--- musicf/static/script.js
+++ musicf/static/script.js
@@ -189,3 +189,139 @@ showMoreMusicBtn.addEventListener("click", () => {
 closeMoreMusicBtn.addEventListener("click", () => {
     showMoreMusicBtn.click();
 })
+
+const queuedMusic = []
+let queueIdx = 0
+let musicIdx = -1
+
+function showRecommendations(){
+    let current_song_details = window.localStorage.getItem("current_playing")
+    let current = JSON.parse(current_song_details)
+    fetch("http://127.0.0.1:5000/api/get_recommendations/"+current.name)
+    .then(res => res.json())
+    .then(d => {
+        const ulTag = document.querySelector(".recommendations ul")
+        for (let i = 0; i < d.results.length; i++){
+            const time = getTiming(d.results[i].duration);
+            let liTag = `
+                <li>
+                    <div class="search-details">
+                        <img src="${d.results[i].image}" alt="" height="40px" width="40px">
+                        <div class="result-details">
+                            <h5>${d.results[i].name}</h5>
+                            <p>${d.results[i].artists}</p>
+                        </div>
+                    </div>
+                    <div class="extras">
+                        <span id="song-${i}" class="material-symbols-outlined" onclick="addToQueue(${i}, ['${d.results[i].name.replace("'", "")}', '${d.results[i].image}', '${d.results[i].uri}', '${d.results[i].duration}','${d.results[i].artists}'])">queue_music</span>
+                        <div class="search-time">${time}</div>
+                    </div>
+                </li>
+            `
+            console.log(liTag)
+            ulTag.insertAdjacentHTML("beforeend", liTag);
+        }
+    })
+    
+    document.querySelector(".recommendations").classList.toggle("show")
+    if (document.getElementById("rccBtn").innerText === "Show Recommendations"){
+        document.getElementById("rccBtn").innerText = "Hide Recommendations";
+    }
+    else{
+        document.getElementById("rccBtn").innerText = "Show Recommendations";
+        
+    }
+}
+
+
+function addToQueue(idx, list){
+    const ulTag = musicList.querySelector("ul");
+    let currentMin = Math.floor(Number((list[3]/1000) / 60));
+    let currentSec = Math.floor(Number(((list[3]/1000)) % 60));
+    if (currentSec < 10){
+        currentSec = `0${currentSec}`;
+    }
+    queuedMusic[queueIdx] = {
+        name: list[0],
+        img_url: list[1],
+        uri: list[2],
+        duration: list[3],
+        artists: list[4]
+    }
+    queueIdx++;
+    let liTag = `
+        <li>
+            <div class="row">
+                <span>${list[0]}</span>
+                <p>${list[4]}</p>
+            </div>
+            <span class="audio-duration">${currentMin}:${currentSec}</span>
+        </li>
+    `
+    ulTag.insertAdjacentHTML("beforeend", liTag);
+    const tarSpan = document.getElementById(`song-${idx}`)
+    tarSpan.innerText = playlist_add_check
+    console.log(queuedMusic)
+}
+
+try{
+    document.getElementById("next").addEventListener("click", () => {
+        nextMusic();
+    })
+}
+catch(err){
+    console.log(err)
+}
+
+try{
+    document.getElementById("prev").addEventListener("click", () => {
+        prevMusic();
+    })
+}
+catch(err){
+    console.log(err)
+}
+
+
+function loadMusic(){
+    window.localStorage.duration = Number(queuedMusic[musicIdx].duration / 1000);
+    window.localStorage.current_time = 0;
+    window.localStorage.progress_ms = 0;
+    const playArea = document.querySelector(".player");
+    const songName = playArea.querySelector(".show-area .song-details .song-name");
+    const artistName = playArea.querySelector(".show-area .song-details .artists-name");
+    const img = playArea.querySelector(".img-area img")
+    const time_dur = playArea.querySelector(".show-area .progress-area .timer .duration")
+    let currentMin = Math.floor(Number((queuedMusic[musicIdx].duration/1000) / 60));
+    let currentSec = Math.floor(Number(((queuedMusic[musicIdx].duration/1000)) % 60));
+    if (currentSec < 10){
+        currentSec = `0${currentSec}`;
+    }
+    songName.innerText = queuedMusic[musicIdx].name;
+    artistName.innerText = queuedMusic[musicIdx].artists;
+    img.src = `${queuedMusic[musicIdx].img_url}`
+    time_dur.innerText = `${currentMin}:${currentSec}`
+    _pauseMusic();
+    updateProgressBar();
+}
+
+
+function clearProgress(){
+    window.localStorage.progress_ms = 0;
+}
+
+function nextMusic(){
+    if ((musicIdx + 1) < queueIdx){
+        musicIdx++;
+        loadMusic();
+        setTimeout(clearProgress, 5000);
+    }
+}
+
+function prevMusic(){
+    if((musicIdx - 1) >= 0){
+        musicIdx--;
+        loadMusic();
+        setTimeout(clearProgress, 5000);
+    }
+}
